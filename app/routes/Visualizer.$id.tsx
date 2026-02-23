72ec5ef177d17a52c9ce22115bc0d363da1fa0d9
dhruv jain <dhruvjain78791999@gmail.com>
2026-02-23 15:48:44 +0530

    implement drag and drop uploader

---
+++ app/routes/Visualizer.$id.tsx
@@ -0,0 +1,8 @@
+
+const Visualizer = () => {
+  return (
+    <div>Visualizer.$id</div>
+  )
+}
+
+export default Visualizer
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,8 @@ import type { Route } from "./+types/home";
 
 import Navbar from "components/Navbar";
 import Button from "components/ui/Button";
+import Upload from "components/Upload";
+import { useNavigate } from "react-router";
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -12,6 +14,14 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const navigate = useNavigate()
+
+  const handleUploadComplete = async(base64Image:string)=>{
+     const newId = Date.now().toString();
+     navigate(`/visualizer/${newId}`)
+
+     return true
+  }
   return (
     <div className="home">
       <Navbar />
@@ -54,7 +64,7 @@ export default function Home() {
               <p>Supports JPG,PNG,formats up to 10MB</p>
 
             </div>
-            <p>Upload images</p>
+            <Upload onComplete={handleUploadComplete}/>
 
           </div>
 
